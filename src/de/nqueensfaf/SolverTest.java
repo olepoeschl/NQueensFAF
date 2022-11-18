c37366fd0f0aa65d0f6bd2440b975cc69a1c2f94
Ole Pöschl <62295828+olepoeschl@users.noreply.github.com>
2022-11-18 20:55:15 +0100

    Removed useless test code

--- src/de/nqueensfaf/SolverTest.java
+++ src/de/nqueensfaf/SolverTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import de.nqueensfaf.compute.CpuSolver;
-import de.nqueensfaf.compute.GpuSolver;
-
 class SolverTest {
 
 	Solver solver;
@@ -32,7 +29,7 @@ class SolverTest {
 	// Manual test
 //	@Test
 //	void testCpuSolver() {
-//		CpuSolver s = new CpuSolver();
+//		de.nqueensfaf.compute.CpuSolver s = new de.nqueensfaf.compute.CpuSolver();
 //		s.setN(16);
 //		s.setThreadcount(1);
 //		s.addTerminationCallback(() -> {
@@ -44,16 +41,10 @@ class SolverTest {
 	// Manual test
 	@Test
 	void testGpuSolver() {
-//		try {
-//			// for making sure the profiler (VisualVM) is ready
-//			Thread.sleep(5000);
-//		} catch (InterruptedException e1) {
-//			e1.printStackTrace();
-//		}
-		GpuSolver s = new GpuSolver();
+		de.nqueensfaf.compute.GpuSolver s = new de.nqueensfaf.compute.GpuSolver();
 		s.setDevice(0);
-		s.setProgressUpdatesEnabled(true);
-		s.setN(20);
+		s.setProgressUpdatesEnabled(false);
+		s.setN(18);
 		new Thread(() -> {
 			while(true) {
 				if(s.getGlobalWorkSize() == 0) {
@@ -76,132 +67,4 @@ class SolverTest {
 		});
 		s.solve();
 	}
-	
-	/*
-	// Manual test
-	@Test
-	void test() {
-		solver = new MockSolver();
-		solver.setN(5);
-		solver.addInitializationCallback(() -> System.out.println("starte..."));
-		solver.addTerminationCallback(() -> System.out.println("fertig!"));
-		solver.setOnTimeUpdateCallback((duration) -> {
-			System.out.println("Duration: " + duration/1000 + "." + duration%1000);
-		});
-		solver.setTimeUpdateDelay(500);
-		solver.setOnProgressUpdateCallback((progress, solutions) -> {
-			System.out.println("Progress: " + progress + ", solutions: " + solutions);
-		});
-		solver.setProgressUpdateDelay(1000);
-		solver.solve();
-		
-		solver.reset();
-		solver.solveAsync();
-		while(solver.isIdle() || solver.isInitializing());
-		while(solver.isRunning()) {
-			System.out.println("läuft");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			if(solver.getDuration() > 2500)
-				break;
-		}
-		System.out.println("warte auf beendigung...");
-		try {
-			solver.waitFor();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-	void testFail() {
-		// setN
-		assertThrows(IllegalArgumentException.class, () -> new MockSolver().setN(-2));
-		assertThrows(IllegalArgumentException.class, () -> new MockSolver().setN(32));
-		solver = new MockSolver();
-		solver.setN(2);
-		solver.solveAsync();
-		assertThrows(IllegalStateException.class, () -> solver.setN(15));
-		try {
-			solver.waitFor();
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
-		}
-		
-		// set<X>UpdateDelay
-		assertThrows(IllegalArgumentException.class, () -> solver.setTimeUpdateDelay(-7));
-		assertThrows(IllegalArgumentException.class, () -> solver.setProgressUpdateDelay(-13));
-		
-		// solve, solveAsync
-		solver = new MockSolver();
-		assertThrows(IllegalStateException.class, () -> solver.solve());
-		solver.setN(1);
-		solver.solveAsync();
-		assertThrows(IllegalStateException.class, () -> solver.solve());
-		assertThrows(IllegalStateException.class, () -> solver.solveAsync());
-		try {
-			solver.waitFor();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		// initialization, termination
-		assertThrows(IllegalArgumentException.class, () -> solver.addInitializationCallback(null));
-		assertThrows(IllegalArgumentException.class, () -> solver.addTerminationCallback(null));
-	}
-	
-	class MockSolver extends Solver {
-		int total, done;
-		long start, end;
-		
-		@Override
-		protected void run() {
-			total = 100*N;
-			start = System.currentTimeMillis();
-			for(done = 0; done < total; done++) {
-				end = System.currentTimeMillis();
-				try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		@Override
-		public void store(String filename) {}
-		@Override
-		public void restore(String filename) {}
-
-		@Override
-		public void reset() {
-			done = 0;
-			start = 0;
-			end = 0;
-		}
-		
-		@Override
-		public long getDuration() {
-			return end - start;
-		}
-
-		@Override
-		public float getProgress() {
-			return ((float) done) / total;
-		}
-
-		@Override
-		public long getSolutions() {
-			return done*10;
-		}
-
-		@Override
-		public boolean isRestored() {
-			return false;
-		}
-	}
-	*/
 }
