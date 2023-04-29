e4c1a1d6ad10588c7ac421636f95b5daa7500f1f
Ole Pöschl <62295828+olepoeschl@users.noreply.github.com>
2023-04-29 13:32:40 +0200

    Replaced the SolverTest class by a normal class

---
+++ src/de/nqueensfaf/Test.java
@@ -0,0 +1,17 @@
+package de.nqueensfaf;
+
+import de.nqueensfaf.compute.CpuSolver;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		CpuSolver s = new CpuSolver();
+		s.setN(16);
+		s.setThreadcount(1);
+		s.addTerminationCallback(() -> {
+			System.out.println(s.getSolutions() + " solutions found in " + s.getDuration() + "ms");
+		});
+		s.solve();
+	}
+	
+}
