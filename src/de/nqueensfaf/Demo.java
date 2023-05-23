b83254f8772a09d7f08107643c0922eec7364bc1
Ole Pöschl <62295828+olepoeschl@users.noreply.github.com>
2023-05-23 20:13:17 +0200

    clean simple demo

--- src/de/nqueensfaf/Demo.java
+++ src/de/nqueensfaf/Demo.java
@@ -1,98 +1,27 @@
 package de.nqueensfaf;
 
-import java.io.IOException;
-
 import de.nqueensfaf.compute.GPUSolver;
 import de.nqueensfaf.compute.Solver;
-import de.nqueensfaf.config.Config;
 import de.nqueensfaf.config.DeviceConfig;
 
 public class Demo {
 	
 	public static void main(String[] args) {
 		run();
-//		store();
-//		restore();
-//		rerestore();
 	}
 	
 	static void run() {
 		GPUSolver s =  Solver.createGPUSolver();
-		s.setN(18);
 		var c = new DeviceConfig(0, 64, 6, 5);
 		s.setDeviceConfigs(c);
-		s.setTerminationCallback((self) -> {
-			System.out.println(self.getSolutions() + " solutions found in " + self.getDuration() + "ms");
-		});
-		s.solve();
-	}
-	
-	static void store() {
-		GPUSolver s = Solver.createGPUSolver();
 		s.setN(20);
-//		s.setDeviceConfigs(GPUSolver.ALL_DEVICES);
-//		s.setDeviceConfigs(new DeviceConfig(1, 24, 6, 1), new DeviceConfig(0, 64, 6, 9));
-		s.setDeviceConfigs(new DeviceConfig(0, 24, 6, 1), new DeviceConfig(1, 64, 6, 9));
-//		s.setDeviceConfigs(new DeviceConfig(0, 64, 6, 5));
-//		s.setDeviceConfigs(new DeviceConfig(1, 24, 6, 5));
-		s.setTerminationCallback((self) -> {
-			System.out.println(self.getSolutions() + " solutions found in " + self.getDuration() + "ms");
-		});
 		s.setOnProgressUpdateCallback((progress, solutions, duration) -> {
-			System.out.println("progress: " + progress + ", solutions: " + solutions + ", duration: " + duration + "ms");
-			if(progress > 0.1)
-				try {
-					s.store("test_task.json");
-					System.exit(0);
-				} catch (IllegalArgumentException | IOException e) {
-					e.printStackTrace();
-				}
+			System.out.format("\r\tprogress: %1.10f\tsolutions: %18d\tduration: %dms", progress, solutions, duration);
+		});
+		s.setTerminationCallback((self) -> {
+			System.out.println();
+			System.out.println(self.getSolutions() + " solutions found in " + self.getDuration() + "ms");
 		});
 		s.solve();
-	}
-	
-	static void restore() {
-		GPUSolver s = Solver.createGPUSolver();
-//		s.setDeviceConfigs(GPUSolver.ALL_DEVICES);
-		s.setDeviceConfigs(new DeviceConfig(1, 24, 6, 1), new DeviceConfig(0, 64, 6, 9));
-		s.setDeviceConfigs(new DeviceConfig(0, 64, 6, 5));
-//		s.setDeviceConfigs(new DeviceConfig(1, 24, 6, 5));
-		s.setTerminationCallback((self) -> {
-			System.out.println(self.getSolutions() + " solutions found in " + self.getDuration() + "ms");
-		});
-		s.setOnProgressUpdateCallback((progress, solutions, duration) -> {
-			System.out.println("progress: " + progress + ", solutions: " + solutions + ", duration: " + duration + "ms");
-			if(progress > 0.8)
-				try {
-					s.store("test_task.json");
-					System.exit(0);
-				} catch (IllegalArgumentException | IOException e) {
-					e.printStackTrace();
-				}
-		});
-		try {
-			s.inject("test_task.json");
-		} catch (ClassNotFoundException | ClassCastException | IllegalArgumentException | IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	static void rerestore() {
-		GPUSolver s = Solver.createGPUSolver();
-//		s.setDeviceConfigs(GPUSolver.ALL_DEVICES);
-		s.setDeviceConfigs(new DeviceConfig(1, 24, 6, 1), new DeviceConfig(0, 64, 6, 9));
-//		s.setDeviceConfigs(new DeviceConfig(0, 64, 6, 5));
-//		s.setDeviceConfigs(new DeviceConfig(1, 24, 6, 5));
-		s.setTerminationCallback((self) -> {
-			System.out.println(self.getSolutions() + " solutions found in " + self.getDuration() + "ms");
-		});
-		s.setOnProgressUpdateCallback((progress, solutions, duration) -> {
-			System.out.println("progress: " + progress + ", solutions: " + solutions + ", duration: " + duration + "ms");
-		});
-		try {
-			s.inject("test_task.json");
-		} catch (ClassNotFoundException | ClassCastException | IllegalArgumentException | IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
