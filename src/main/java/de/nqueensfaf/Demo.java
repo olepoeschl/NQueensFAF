c210a5b719a001d3eac0bff430b0249d0c9b9be7
Ole Pöschl <62295828+olepoeschl@users.noreply.github.com>
2023-06-19 22:00:21 +0200

    Update Demo.java

--- src/main/java/de/nqueensfaf/Demo.java
+++ src/main/java/de/nqueensfaf/Demo.java
@@ -1,73 +1,19 @@
 package de.nqueensfaf;
 
-import java.io.File;
-import java.io.IOException;
-
-import com.fasterxml.jackson.core.exc.StreamWriteException;
-import com.fasterxml.jackson.databind.DatabindException;
-
-import de.nqueensfaf.config.Config;
-import de.nqueensfaf.config.ConfigOld;
-import de.nqueensfaf.config.DeviceConfig;
 import de.nqueensfaf.impl.CPUSolver;
-import de.nqueensfaf.impl.GPUSolver;
-import de.nqueensfaf.impl.GPUSolver.GPUSolverConfig;
 
 public class Demo {
 
-    public static void main(String[] args) throws StreamWriteException, DatabindException, IOException, IllegalArgumentException, IllegalAccessException {
-//	run();
-	GPUSolver s = new GPUSolver().config((config) -> {
-	    config.presetQueens = 8;
-	    try {
-		config.writeTo(new File("config.txt"));
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
-	});
-    }
-
-    static void run() {
-	// @formatter:off
-	GPUSolver s = new GPUSolver()
-		.config((config) -> {
-//		    try {
-//			config.from(new File("config.txt"));
-//		    } catch (IOException e) {
-//			e.printStackTrace();
-//		    }
-		    config.deviceConfigs = new DeviceConfig[] {
-			new DeviceConfig(0, 64, 1, 1_000_000_000)
-		    };
-		    config.presetQueens = 6;
-//		    try {
-//			config.writeTo(new File("config.txt"));
-//		    } catch (IOException e) {
-//			e.printStackTrace();
-//		    }
-		})
-		.onInit((self) -> {
-		    System.out.println("Starting solver for board size " + self.getN() + "...");
-		})
-		.onUpdate((self, progress, solutions, duration) -> {
-		    System.out.format("\r\tprogress: %1.10f\tsolutions: %18d\tduration: %dms", progress, solutions, duration);
-		    if (duration > 3000) {
-			new Thread(() -> {
-        		    try {
-        			self.store("hi.txt");
-        		    } catch (IllegalArgumentException | IOException e) {
-        			e.printStackTrace();
-        		    }
-			}).start();
-			System.exit(0);
-		    }
-		})
-		.onFinish((self) -> {
-		    System.out.println();
-		    System.out.println(self.getSolutions() + " solutions found in " + self.getDuration() + "ms");
-		})
-		.setN(18);
-	// @formatter:on
-	s.solve();
+    public static void main(String[] args) {
+	CPUSolver s = new CPUSolver()
+        	.config(config -> {
+        	    config.threadcount = 2;
+        	    config.updateInterval = 300;
+        	})
+        	.onInit(self -> System.out.println("Starting Solver for board size " + self.getN() + "..."))
+        	.onUpdate((self, progress, solutions, duration) -> System.out.println("progress: " + progress + " solutions: " + solutions + " duration: " + duration))
+        	.onFinish(self -> System.out.println("Found " + self.getSolutions() + " solutions in " + self.getDuration() + " ms"))
+        	.setN(16)
+        	.solve();
     }
 }
