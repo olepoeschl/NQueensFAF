47437d247aa2ec16583fa10ff4940f007b0f325b
Ole Pöschl <62295828+olepoeschl@users.noreply.github.com>
2023-09-12 10:48:59 +0200

    Update CudaSolverTest.java

--- src/main/java/de/nqueensfaf/impl/CudaSolverTest.java
+++ src/main/java/de/nqueensfaf/impl/CudaSolverTest.java
@@ -5,7 +5,7 @@ public class CudaSolverTest {
     public static void main(String[] args) {
 	GPUSolverCUDA c = new GPUSolverCUDA();
 	c.setN(18).onFinish((self) -> {
-	   System.out.println("solutions: " + self.getSolutions()); 
+	   System.out.println("solutions: " + self.getSolutions() + ", duration: " + self.getDuration() + "ms"); 
 	})
 	.onUpdate((self, progress, solutions, duration) -> {
 	    System.out.println("progress: " + progress + ", solutions: " + solutions + ", duration: " + duration + "ms");
