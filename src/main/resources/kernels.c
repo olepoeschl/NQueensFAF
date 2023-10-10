6ff433a16a4e0c1e9d16611ca566ca62d4853740
Ole Pöschl <62295828+olepoeschl@users.noreply.github.com>
2023-10-10 13:08:37 +0200

    Less private variables in kernel, const keyword

--- src/main/resources/kernels.c
+++ src/main/resources/kernels.c
@@ -15,21 +15,21 @@ struct constellation {
 // Nvidia kernel
 kernel void nqfaf_nvidia(global struct constellation *constellation_arr, global long *result) {
 	// gpu intern indice
-	int g_id = get_global_id(0);			// global thread id 
-	int l_id = get_local_id(0);  			// local thread id within workgroup
+//	int g_id = get_global_id(0);			// global thread id 
+//	int l_id = get_local_id(0);  			// local thread id within workgroup
 
-	struct constellation c = constellation_arr[g_id];
+	const struct constellation c = constellation_arr[get_global_id(0)];
 	
 	// variables		
-	uint L = 1 << (N-1);				// queen at the left border of the board (right border is represented by 1) 										
+	const uint L = 1 << (N-1);				// queen at the left border of the board (right border is represented by 1) 										
 	// start_jkl_arr contains [6 queens free][5 queens for start][5 queens for i][5 queens for j][5 queens for k][5 queens for l] 
-	int start = c.start_ijkl >> 20;		
+	const int start = c.start_ijkl >> 20;		
 	if(start == 69) {				// if we have a pseudo constellation we do nothing 
 		return;
 	}
-	int j = (c.start_ijkl >> 10) & 31;	// queen in last row at position j
-	int k = (c.start_ijkl >> 5) & 31;	// in row k queen at left border, in row l queen at right border
-	int l = c.start_ijkl & 31;
+//	int j = (c.start_ijkl >> 10) & 31;	// queen in last row at position j
+//	int k = (c.start_ijkl >> 5) & 31;	// in row k queen at left border, in row l queen at right border
+//	int l = c.start_ijkl & 31;
 
 	// describe the occupancy of the board 
 	uint ld = c.ld;				// left diagonals, 1 means occupied
@@ -42,26 +42,26 @@ kernel void nqfaf_nvidia(global struct constellation *constellation_arr, global 
 	// and also the left and right column 
 	// in row k only L is free and in row l only 1 is free 
 	local uint jkl_queens[N];
-	uint rdiag = (L >> j) | (L >> (N-1-k));		// the rd from queen j and k with respect to the last row
-	uint ldiag = (L >> j) | (L >> l);		// the ld from queen j and l with respect to the last row
-	if(l_id == 0) {
+	uint rdiag = (L >> ((c.start_ijkl >> 10) & 31)) | (L >> (N-1-((c.start_ijkl >> 5) & 31)));		// the rd from queen j and k with respect to the last row
+	uint ldiag = (L >> ((c.start_ijkl >> 10) & 31)) | (L >> (c.start_ijkl & 31));		// the ld from queen j and l with respect to the last row
+	if(get_local_id(0) == 0) {
 		for(int a = 0;a < N; a++){			// we also occupy the left and right border 
 			jkl_queens[N-1-a] = (ldiag >> a) | (rdiag << a) | L | 1;
 		}
 	}
-	ldiag = L >> k;					// ld from queen l with respect to the first row 
-	rdiag = 1 << l;					// ld from queen k with respect to the first row 
-	if(l_id == 0) {
-		for(int a = 0;a < N; a++){
+	ldiag = L >> ((c.start_ijkl >> 5) & 31);					// ld from queen l with respect to the first row 
+	rdiag = 1 << (c.start_ijkl & 31);					// ld from queen k with respect to the first row 
+	if(get_local_id(0) == 0) {
+		for(int a = 0; a < N; a++){
 			jkl_queens[a] |= (ldiag << a) | (rdiag >> a);
 		}
-		jkl_queens[k] = ~L;
-		jkl_queens[l] = ~1; 
+		jkl_queens[((c.start_ijkl >> 5) & 31)] = ~L;
+		jkl_queens[(c.start_ijkl & 31)] = ~1; 
 	}
 	barrier(CLK_LOCAL_MEM_FENCE);			// avoid corrupt memory behavior 
 
 	ld &= ~(ldiag << start);			// remove queen k from ld 
-	if(l != N-1)					// only remove queen k from rd, if no queen in corner (N-1,N-1)
+	if((c.start_ijkl & 31) != N-1)					// only remove queen k from rd, if no queen in corner (N-1,N-1)
 		rd &= ~(rdiag >> start);			// otherwise we continue in row N-1 and find too many solutions 
 
 	// initialize current row as start and solutions as 0
@@ -74,7 +74,7 @@ kernel void nqfaf_nvidia(global struct constellation *constellation_arr, global 
 	// each row of queens contains the queens of the board of one workitem 
 	// local arrays are faster 
 	local uint queens[WORKGROUP_SIZE][N];		// for remembering the queens for all rows for all boards in the work-group 
-	queens[l_id][start] = queen;			// we already calculated the first queen in the start row 
+	queens[get_local_id(0)][start] = queen;			// we already calculated the first queen in the start row 
 
 	// going forward (setting a queen) or backward (removing a queen)? 										
 	int direction = 0;
@@ -86,7 +86,7 @@ kernel void nqfaf_nvidia(global struct constellation *constellation_arr, global 
 		if(free) {					// if there are free slots in the current row 
 			direction = 1;					// we are going forwards 
 			queen = -free & free;				// this is the next free slot for a queen (searching from the right border) in the current row
-			queens[l_id][row] = queen;			// remember the queen 
+			queens[get_local_id(0)][row] = queen;			// remember the queen 
 			row++;						// increase row counter 
 
 			ld_mem = ld_mem << 1 | ld >> 31;		// place the queen in the diagonals and shift them and remember the diagonals leaving the board 
@@ -97,7 +97,7 @@ kernel void nqfaf_nvidia(global struct constellation *constellation_arr, global 
 		else {						// if the row is completely occupied 
 			direction = 0;					// we are going backwards 
 			row--;						// decrease row counter 
-			queen = queens[l_id][row];			// recover the queen in order to remove it 
+			queen = queens[get_local_id(0)][row];			// recover the queen in order to remove it 
 
 			ld = ((ld >> 1) | (ld_mem << 31)) & ~queen;	// shift diagonals one back, remove the queen and insert the diagonals that had left the board 
 			rd = ((rd << 1) | (rd_mem >> 31)) & ~queen;
@@ -118,7 +118,7 @@ kernel void nqfaf_nvidia(global struct constellation *constellation_arr, global 
 		if(free) {					// if there are free slots in the current row 
 			direction = 1;					// we are going forwards 
 			queen = -free & free;				// this is the next free slot for a queen (searching from the right border) in the current row
-			queens[l_id][row] = queen;			// remember the queen 
+			queens[get_local_id(0)][row] = queen;			// remember the queen 
 			row++;						// increase row counter 
 
 			ld_mem = ld_mem << 1 | ld >> 31;		// place the queen in the diagonals and shift them and remember the diagonals leaving the board 
@@ -129,7 +129,7 @@ kernel void nqfaf_nvidia(global struct constellation *constellation_arr, global 
 		else {						// if the row is completely occupied 
 			direction = 0;					// we are going backwards 
 			row--;						// decrease row counter 
-			queen = queens[l_id][row];			// recover the queen in order to remove it 
+			queen = queens[get_local_id(0)][row];			// recover the queen in order to remove it 
 
 			ld = ((ld >> 1) | (ld_mem << 31)) & ~queen;	// shift diagonals one back, remove the queen and insert the diagonals that had left the board 
 			rd = ((rd << 1) | (rd_mem >> 31)) & ~queen;
@@ -143,7 +143,7 @@ kernel void nqfaf_nvidia(global struct constellation *constellation_arr, global 
 		if(row == N-1)					// increase the solutions, if we are in the last row 
 			solutions++;
 	}
-	result[g_id] = solutions;			// number of solutions of the work item 
+	result[get_global_id(0)] = solutions;			// number of solutions of the work item 
 }
 
 // AMD kernel
