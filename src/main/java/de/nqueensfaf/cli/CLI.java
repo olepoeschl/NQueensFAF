2b18de1bb59a03d11b639f216e93e6a17299ceda
opoeschl@hs-mittweida.de <opoeschl@hs-mittweida.de>
2024-01-09 01:03:46 +0100

    Started rewriting cli

--- src/main/java/de/nqueensfaf/cli/CLI.java
+++ src/main/java/de/nqueensfaf/cli/CLI.java
@@ -1,6 +1,7 @@
 package de.nqueensfaf.cli;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 import com.github.freva.asciitable.AsciiTable;
@@ -13,10 +14,12 @@ import de.nqueensfaf.impl.GPUSolver;
 import de.nqueensfaf.impl.SolverState;
 import de.nqueensfaf.impl.SymSolver;
 import picocli.CommandLine;
+import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Spec;
 import picocli.CommandLine.TypeConversionException;
 
@@ -25,29 +28,49 @@ public class CLI implements Runnable {
 
     @Spec
     CommandSpec spec;
-
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "show this help message")
-    private boolean help;
     
-    @Option(names = { "-d",
-	    "--show-devices" }, required = false, description = "show a list of all available OpenCL devices")
-    private boolean showAvailableDevices;
+    @ArgGroup(exclusive = true, multiplicity = "1")
+    private NorTask nOrTask;
 
-    private File taskFile;
-    @Option(names = { "-t",
-	    "--task" }, paramLabel = "FILE", required = false, description = "absolute path to the file containing the task")
-    public void setTaskFile(File taskFile) {
-	if (!taskFile.exists()) {
-	    throw new ParameterException(spec.commandLine(), "invalid task file: file not found");
+    private static class NorTask {
+	private int n;
+	@Parameters(description = "size of the chess board")
+	public void sizeOfBoard(String input) {
+	    try {
+		n = Integer.parseInt(input);
+	    } catch(NumberFormatException e) {
+		throw new TypeConversionException("invalid board size: '" + input + "' is not an integer");
+	    }
 	}
-	this.taskFile = taskFile;
+
+	private SolverState state;
+	@Parameters(description = "path to the solver state file")
+	public void pathToSolverStateFile(String input) {
+	    try {
+		state = SolverState.load(input);
+	    } catch (IOException e) {
+		throw new TypeConversionException("invalid path: " + e.getMessage());
+	    }
+	}
+    }
+    
+    @Option(names = { "-l", "--list-gpus" }, required = false, description = "show a list of all available GPUs")
+    public void listGpus() {
+	// TODO
+    }
+    
+    int updateInterval;
+    @Option(names = { "-u", "--update-interval" }, required = false, description = "delay between progress updates")
+    public void updateInterval() {
+	// TODO
     }
 
-    @Option(names = { "-n", "--board-size" }, paramLabel = "INT", required = false, description = "board size")
-    private int n = -69;
-
-    @Option(names = { "-g", "--gpu" }, required = false, description = "execute on GPU('s)")
-    private boolean executeOnGpu;
+    private String[] gpus;
+    private int[] workgroupSizes;
+    @Option(names = { "-g", "--gpus" }, required = false, description = "choose and configure GPUs for copmuting")
+    public void gpuConfigs(String input) {
+	// TODO
+    }
 
     // for printing the progress in the progress callback
     private final String progressStringFormat = "\r%c\tprogress: %1.10f\tsolutions: %18d\tduration: %12s";
