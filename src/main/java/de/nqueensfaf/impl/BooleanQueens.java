ee62972a3e3bd06ed15accceddff13148b3aa136
Tim Pöschl <tim.poeschl@math.tu-freiberg.de>
2024-02-20 16:25:21 +0100

    boolean queen indicator solver

---
+++ src/main/java/de/nqueensfaf/impl/BooleanQueens.java
@@ -0,0 +1,112 @@
+package de.nqueensfaf.impl;
+
+import java.util.Arrays;
+
+import de.nqueensfaf.Solver;
+
+public class BooleanQueens extends Solver{
+
+	private int N;
+	private long start, sol, time;
+	private boolean[][] queens;
+	
+	public BooleanQueens(int N) {
+		this.N = N;
+		this.sol = 0;
+		this.queens = new boolean[N][N];
+	}
+	
+	private void solve_board() {
+		this.start = System.currentTimeMillis();
+		this.sq(0);
+		this.time = System.currentTimeMillis() - start;
+	}
+	private void sq(int row) {
+		if(row == N) {
+			sol++;
+//			print_queens();
+			return;
+		}
+		for(int col = 0; col < N; col++) {
+			if(occupied(row, col) == false) {
+				queens[row][col] = true;
+				sq(row+1);
+				queens[row][col] = false;
+			}
+		}
+	}
+	private boolean occupied(int row, int col) {
+		for(int k = row; k >= 0; k--) {
+			if (queens[k][col])
+				return true;
+		}
+		int k = 0;
+		while(row-k>=0 && col-k>=0) {
+			if (queens[row-k][col-k])
+				return true;
+			k++;
+		}
+		k = 0;
+		while(row-k>=0 && col+k<=N-1) {
+			if(queens[row-k][col+k])
+				return true;
+			k++;
+		}
+		return false;
+	}
+	
+//	private void print_queens() {
+//		for(int i = 0; i < N; i++) {
+//			for(int j = 0; j < N; j++) {
+//				System.out.print('[');
+//				if (queens[i] == j)
+//					System.out.print('X');
+//				else
+//					System.out.print(' ');
+//				System.out.print(']');
+//			}
+//			System.out.print('\n');
+//		}
+//		System.out.print('\n');
+//	}
+//	private void print_occupancy(boolean[][] board) {
+//		for(int i = 0; i < N; i++) {
+//			for(int j = 0; j < N; j++) {
+//				System.out.print('[');
+//				if (board[i][j] == true)
+//					System.out.print('X');
+//				else
+//					System.out.print(' ');
+//				System.out.print(']');
+//			}
+//			System.out.print('\n');
+//		}
+//		System.out.print('\n');
+//	}
+
+	@Override
+	public long getDuration() {
+		return System.currentTimeMillis() - start;
+	}
+	@Override
+	public float getProgress() {
+		return 0;
+	}
+	@Override
+	public long getSolutions() {
+		return sol;
+	}
+	@Override
+	protected void run() {
+		return;
+	}
+
+	public static void main(String[] args){
+		for(int N = 1; N <= 14; N++) {
+			BooleanQueens B = new BooleanQueens(N);
+			B.solve_board();
+			System.out.println("N = " + B.N + ": " + B.sol + " solutions in " + B.time + "ms");
+		}
+	}
+
+}
