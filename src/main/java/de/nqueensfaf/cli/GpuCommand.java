f3798aa4444d08a3eee41550a35b189d3f1439d1
opoeschl@hs-mittweida.de <opoeschl@hs-mittweida.de>
2024-03-11 13:03:22 +0100

    Prettier cli (same as for client)

--- src/main/java/de/nqueensfaf/cli/GpuCommand.java
+++ src/main/java/de/nqueensfaf/cli/GpuCommand.java
@@ -1,14 +1,13 @@
 package de.nqueensfaf.cli;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
 
 import com.github.freva.asciitable.AsciiTable;
 import com.github.freva.asciitable.Column;
 import com.github.freva.asciitable.HorizontalAlign;
 
 import de.nqueensfaf.impl.GpuSolver;
-import de.nqueensfaf.impl.GpuSolver.Gpu;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.ParentCommand;
@@ -22,75 +21,84 @@ public class GpuCommand implements Runnable {
     @Option(names = { "-p", "--preset-queens" }, required = false, defaultValue = "6", description = "How many queens should be placed for a start positions")
     int presetQueens;
 
-    @Option(names = { "-g", "--gpus" }, required = false, split = ",", converter = GpuConverter.class, 
-	    description = "GPUs that should be used in the format of <string_contained_in_name>[:<attr><val>[,:<attr><val>]]"
-	    	+ "\n<attr> can be one of the following: wg, bm, al"
-	    	+ "\n<val> is the value that should be assigned to the attribute, if the attribute expects one")
-    GPURequest[] gpu;
-
-    @Option(names = { "-l", "--list-gpus" }, required = false, description = "Print a list of all available GPUs")
-    boolean printGpuList;
-
     private GpuSolver solver;
     
     public GpuCommand() {}
     
     @Override
     public void run() {
-	if(printGpuList) {
-	    var gpus = new GpuSolver().getAvailableGpus();
-	    System.out.println(
-		    AsciiTable.getTable(AsciiTable.BASIC_ASCII, gpus,
-			    Arrays.asList(
-				    new Column().header("Vendor").headerAlign(HorizontalAlign.CENTER)
-				    .dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().vendor()),
-				    new Column().header("Name").headerAlign(HorizontalAlign.CENTER)
-				    .dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().name()))));
-	    return;
-	}
-
 	solver = new GpuSolver();
 	base.applySolverConfig(solver);
 	
 	if(presetQueens != 0)
 	    solver.setPresetQueens(presetQueens);
-	
-	List<Gpu> availableGpus = solver.getAvailableGpus();
-	if(gpu != null) {
-	    for(var requestedGpu : gpu) {
-		if(requestedGpu.useAllMatchingGpus) {
-		    var matchingGpus = availableGpus.stream().filter(gi -> gi.getInfo().name().toLowerCase().contains(requestedGpu.nameContains)).toList();
-		    for(var matchingGpu : matchingGpus) {
-			matchingGpu.getConfig().setBenchmark(requestedGpu.benchmarkScore);
-			matchingGpu.getConfig().setWorkgroupSize(requestedGpu.workgroupSize);
-			solver.gpuSelection().add(matchingGpu);
-		    }
-		} else {
-		    var matchingGpu = availableGpus.stream().filter(gi -> gi.getInfo().name().toLowerCase().contains(requestedGpu.nameContains)).findFirst().get();
-		    matchingGpu.getConfig().setBenchmark(requestedGpu.benchmarkScore);
-		    matchingGpu.getConfig().setWorkgroupSize(requestedGpu.workgroupSize);
-		    solver.gpuSelection().add(matchingGpu);
+
+	var availableGpus = solver.getAvailableGpus();
+	System.out.println(
+		AsciiTable.getTable(AsciiTable.BASIC_ASCII, availableGpus,
+			Arrays.asList(
+				new Column().header("Index").headerAlign(HorizontalAlign.CENTER)
+				.dataAlign(HorizontalAlign.CENTER).with(gpu -> Integer.toString(availableGpus.indexOf(gpu))),
+				new Column().header("Vendor").headerAlign(HorizontalAlign.CENTER)
+				.dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().vendor()),
+				new Column().header("Name").headerAlign(HorizontalAlign.CENTER)
+				.dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().name())
+				)));
+
+	// let user choose which GPUs should be used
+	System.out.println("Please specify the indices of all GPU's you want to use, separated by commata.");
+	System.out.print("> ");
+	try (Scanner scanner = new Scanner(System.in)) {
+	    String input = scanner.nextLine();
+	    var indices = input.split(",");
+
+	    for(int i = 0; i < indices.length; i++) {
+		var configProperties = indices[i].split(":");
+		
+		int index;
+		try {
+		    index = Integer.parseInt(configProperties[0].trim());
+		    if(index < 0 || index >= availableGpus.size())
+			throw new IllegalArgumentException("invalid index: there is no GPU for index " + index);
+		} catch (NumberFormatException e) {
+		    throw new NumberFormatException("invalid index: '" + indices[i].trim() + "' is not an integer");
 		}
+
+		var gpu = availableGpus.get(index);
+		
+		if(configProperties.length > 1) {
+		    for(int j = 1; j < configProperties.length; j++) {
+			String configProperty = configProperties[j];
+			String propertyKey = configProperty.substring(0, 2);
+			String propertyVal = configProperty.substring(2).trim();
+			
+			switch(propertyKey) {
+			case "bm":
+			    try {
+				float benchmark = Float.parseFloat(propertyVal);
+				gpu.getConfig().setBenchmark(benchmark);
+			    } catch (NumberFormatException e) {
+				throw new NumberFormatException("invalid benchmark: '" + propertyVal + "' is not a float");
+			    }
+			    break;
+			case "wg":
+			    try {
+				int workgroupSize = Integer.parseInt(propertyVal);
+				gpu.getConfig().setWorkgroupSize(workgroupSize);
+			    } catch (NumberFormatException e) {
+				throw new NumberFormatException("invalid workgroup size: '" + propertyVal + "' is not an integer");
+			    }
+			    break;
+			default:
+			    throw new IllegalArgumentException("invalid property key: '" + propertyKey + "'");
+			}
+		    }
+		}
+	    
+		solver.gpuSelection().add(gpu);
 	    }
-	} else {
-	    solver.gpuSelection().choose(availableGpus.get(0));
 	}
-	
+
 	solver.solve();
-	System.out.println("(" + base.getUniqueSolutions(solver) + " unique solutions)");
-    }
-
-    static class GPURequest {
-	
-	static final String workgroupSizeKey = "wg", 
-		benchmarkScoreKey = "bm",
-		useAllMatchingGpusKey = "al";
-
-	String nameContains;
-	int workgroupSize;
-	int benchmarkScore;
-	boolean useAllMatchingGpus;
-	
-	public GPURequest() {}
     }
 }
