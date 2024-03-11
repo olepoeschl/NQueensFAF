fe8f91d02e933676a2d8191af73dbf102e9f5a46
opoeschl@hs-mittweida.de <opoeschl@hs-mittweida.de>
2024-03-11 14:38:48 +0100

    Cli option for using default GPU

--- src/main/java/de/nqueensfaf/cli/GpuCommand.java
+++ src/main/java/de/nqueensfaf/cli/GpuCommand.java
@@ -20,6 +20,9 @@ public class GpuCommand implements Runnable {
 
     @Option(names = { "-p", "--preset-queens" }, required = false, defaultValue = "6", description = "How many queens should be placed for a start positions")
     int presetQueens;
+    
+    @Option(names = { "-0", "--use-default-gpu" }, description = "Use the default GPU", defaultValue = "false")
+    boolean useDefaultGpu;
 
     private GpuSolver solver;
     
@@ -33,72 +36,76 @@ public class GpuCommand implements Runnable {
 	if(presetQueens != 0)
 	    solver.setPresetQueens(presetQueens);
 
-	var availableGpus = solver.getAvailableGpus();
-	System.out.println(
-		AsciiTable.getTable(AsciiTable.BASIC_ASCII, availableGpus,
-			Arrays.asList(
-				new Column().header("Index").headerAlign(HorizontalAlign.CENTER)
-				.dataAlign(HorizontalAlign.CENTER).with(gpu -> Integer.toString(availableGpus.indexOf(gpu))),
-				new Column().header("Vendor").headerAlign(HorizontalAlign.CENTER)
-				.dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().vendor()),
-				new Column().header("Name").headerAlign(HorizontalAlign.CENTER)
-				.dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().name())
-				)));
+	if(useDefaultGpu) {
+	    solver.gpuSelection().add(solver.getAvailableGpus().get(0));
+	} else {
+	    var availableGpus = solver.getAvailableGpus();
+	    System.out.println(
+		    AsciiTable.getTable(AsciiTable.BASIC_ASCII, availableGpus,
+			    Arrays.asList(
+				    new Column().header("Index").headerAlign(HorizontalAlign.CENTER)
+				    .dataAlign(HorizontalAlign.CENTER).with(gpu -> Integer.toString(availableGpus.indexOf(gpu))),
+				    new Column().header("Vendor").headerAlign(HorizontalAlign.CENTER)
+				    .dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().vendor()),
+				    new Column().header("Name").headerAlign(HorizontalAlign.CENTER)
+				    .dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().name())
+				    )));
 
-	// let user choose which GPUs should be used
-	System.out.println("Please specify the indices of all GPU's you want to use, separated by commata.");
-	System.out.print("> ");
-	try (Scanner scanner = new Scanner(System.in)) {
-	    String input = scanner.nextLine();
-	    var indices = input.split(",");
+	    // let user choose which GPUs should be used
+	    System.out.println("Please specify the indices of all GPU's you want to use, separated by commata.");
+	    System.out.print("> ");
+	    try (Scanner scanner = new Scanner(System.in)) {
+		String input = scanner.nextLine();
+		var indices = input.split(",");
 
-	    for(int i = 0; i < indices.length; i++) {
-		var configProperties = indices[i].split(":");
-		
-		int index;
-		try {
-		    index = Integer.parseInt(configProperties[0].trim());
-		    if(index < 0 || index >= availableGpus.size())
-			throw new IllegalArgumentException("invalid index: there is no GPU for index " + index);
-		} catch (NumberFormatException e) {
-		    throw new NumberFormatException("invalid index: '" + indices[i].trim() + "' is not an integer");
-		}
+		for(int i = 0; i < indices.length; i++) {
+		    var configProperties = indices[i].split(":");
 
-		var gpu = availableGpus.get(index);
-		
-		if(configProperties.length > 1) {
-		    for(int j = 1; j < configProperties.length; j++) {
-			String configProperty = configProperties[j];
-			String propertyKey = configProperty.substring(0, 2);
-			String propertyVal = configProperty.substring(2).trim();
-			
-			switch(propertyKey) {
-			case "bm":
-			    try {
-				float benchmark = Float.parseFloat(propertyVal);
-				gpu.getConfig().setBenchmark(benchmark);
-			    } catch (NumberFormatException e) {
-				throw new NumberFormatException("invalid benchmark: '" + propertyVal + "' is not a float");
+		    int index;
+		    try {
+			index = Integer.parseInt(configProperties[0].trim());
+			if(index < 0 || index >= availableGpus.size())
+			    throw new IllegalArgumentException("invalid index: there is no GPU for index " + index);
+		    } catch (NumberFormatException e) {
+			throw new NumberFormatException("invalid index: '" + indices[i].trim() + "' is not an integer");
+		    }
+
+		    var gpu = availableGpus.get(index);
+
+		    if(configProperties.length > 1) {
+			for(int j = 1; j < configProperties.length; j++) {
+			    String configProperty = configProperties[j];
+			    String propertyKey = configProperty.substring(0, 2);
+			    String propertyVal = configProperty.substring(2).trim();
+
+			    switch(propertyKey) {
+			    case "bm":
+				try {
+				    float benchmark = Float.parseFloat(propertyVal);
+				    gpu.getConfig().setBenchmark(benchmark);
+				} catch (NumberFormatException e) {
+				    throw new NumberFormatException("invalid benchmark: '" + propertyVal + "' is not a float");
+				}
+				break;
+			    case "wg":
+				try {
+				    int workgroupSize = Integer.parseInt(propertyVal);
+				    gpu.getConfig().setWorkgroupSize(workgroupSize);
+				} catch (NumberFormatException e) {
+				    throw new NumberFormatException("invalid workgroup size: '" + propertyVal + "' is not an integer");
+				}
+				break;
+			    default:
+				throw new IllegalArgumentException("invalid property key: '" + propertyKey + "'");
 			    }
-			    break;
-			case "wg":
-			    try {
-				int workgroupSize = Integer.parseInt(propertyVal);
-				gpu.getConfig().setWorkgroupSize(workgroupSize);
-			    } catch (NumberFormatException e) {
-				throw new NumberFormatException("invalid workgroup size: '" + propertyVal + "' is not an integer");
-			    }
-			    break;
-			default:
-			    throw new IllegalArgumentException("invalid property key: '" + propertyKey + "'");
 			}
 		    }
+
+		    solver.gpuSelection().add(gpu);
 		}
-	    
-		solver.gpuSelection().add(gpu);
 	    }
 	}
-
+	
 	solver.solve();
     }
 }
