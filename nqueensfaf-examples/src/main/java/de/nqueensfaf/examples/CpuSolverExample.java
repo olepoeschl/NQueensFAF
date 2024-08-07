2f5dd99cd3e8640e4b8b8af51a46e87d940549a6
opoeschl@hs-mittweida.de <opoeschl@hs-mittweida.de>
2024-08-07 19:36:23 +0200

    Typo in CpuSolverExample

--- nqueensfaf-examples/src/main/java/de/nqueensfaf/examples/CpuSolverExample.java
+++ nqueensfaf-examples/src/main/java/de/nqueensfaf/examples/CpuSolverExample.java
@@ -9,11 +9,12 @@ public class CpuSolverExample {
     public static void main(String[] args) {
 
 	try (Scanner scanner = new Scanner(System.in)) {
-	    // read input parameters
+	    // prompt the user to enter N
 	    System.out.println("Please enter N.");
 	    System.out.print("> ");
 	    final int n = scanner.nextInt();
 
+	    // prompt the user to enter the number of threads that should be used
 	    System.out.println("Please enter the number of threads that should be used.");
 	    System.out.print("> ");
 	    final int threads = scanner.nextInt();
@@ -24,7 +25,7 @@ public class CpuSolverExample {
 	    cpuSolver.setThreadCount(threads);
 	    
 	    // define callbacks for when the CpuSolver starts and finishes and when it makes progress
-	    cpuSolver.onStart(() -> System.out.printf("Starting GpuSolver for N=%d.\n", n));
+	    cpuSolver.onStart(() -> System.out.printf("Starting CpuSolver for N=%d.\n", n));
 	    cpuSolver.onUpdate((progress, solutions, duration) -> System.out.printf("\t%.2f%% [%d solutions, %.2fs]\n",
 			progress * 100, solutions, duration / 1000f));
 	    cpuSolver.onFinish(() -> System.out.printf("CpuSolver found %d solutions in %d ms for N=%d\n",
