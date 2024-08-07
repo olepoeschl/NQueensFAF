e216c3db3f4f18ba9b7af0dba9a16ad4532ddf63
opoeschl@hs-mittweida.de <opoeschl@hs-mittweida.de>
2024-08-07 20:23:32 +0200

    Comments  MultiGpuExample, cleaner Code in all examples

--- nqueensfaf-examples/src/main/java/de/nqueensfaf/examples/CpuSolverExample.java
+++ nqueensfaf-examples/src/main/java/de/nqueensfaf/examples/CpuSolverExample.java
@@ -1,6 +1,8 @@
 package de.nqueensfaf.examples;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import de.nqueensfaf.impl.CpuSolver;
 
@@ -8,31 +10,35 @@ public class CpuSolverExample {
 
     public static void main(String[] args) {
 
-	try (Scanner scanner = new Scanner(System.in)) {
+	try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
 	    // prompt the user to enter N
 	    System.out.println("Please enter N.");
 	    System.out.print("> ");
-	    final int n = scanner.nextInt();
+	    final int n = Integer.parseInt(reader.readLine().trim());
 
 	    // prompt the user to enter the number of threads that should be used
 	    System.out.println("Please enter the number of threads that should be used.");
 	    System.out.print("> ");
-	    final int threads = scanner.nextInt();
-	    
+	    final int threads = Integer.parseInt(reader.readLine().trim());
+
 	    // instantiate and configure the CpuSolver
 	    final var cpuSolver = new CpuSolver();
 	    cpuSolver.setN(n);
 	    cpuSolver.setThreadCount(threads);
-	    
-	    // define callbacks for when the CpuSolver starts and finishes and when it makes progress
+
+	    // define callbacks for when the CpuSolver starts and finishes and when it makes
+	    // progress
 	    cpuSolver.onStart(() -> System.out.printf("Starting CpuSolver for N=%d.\n", n));
 	    cpuSolver.onUpdate((progress, solutions, duration) -> System.out.printf("\t%.2f%% [%d solutions, %.2fs]\n",
-			progress * 100, solutions, duration / 1000f));
+		    progress * 100, solutions, duration / 1000f));
 	    cpuSolver.onFinish(() -> System.out.printf("CpuSolver found %d solutions in %d ms for N=%d\n",
 		    cpuSolver.getSolutions(), cpuSolver.getDuration(), n));
 
 	    // start the solver
 	    cpuSolver.start();
+
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
 
     }
--- nqueensfaf-examples/src/main/java/de/nqueensfaf/examples/SingleGpuExample.java
+++ nqueensfaf-examples/src/main/java/de/nqueensfaf/examples/SingleGpuExample.java
@@ -1,13 +1,17 @@
 package de.nqueensfaf.examples;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
 
 import com.github.freva.asciitable.AsciiTable;
 import com.github.freva.asciitable.Column;
 import com.github.freva.asciitable.HorizontalAlign;
 
 import de.nqueensfaf.impl.GpuSolver;
+import de.nqueensfaf.impl.GpuSolver.Gpu;
 
 public class SingleGpuExample {
 
@@ -20,40 +24,27 @@ public class SingleGpuExample {
 	    System.err.println("no GPU available, terminating.");
 	    System.exit(0);
 	}
-	
-	// print a table showing all available GPUs
-	System.out.println(AsciiTable.getTable(AsciiTable.BASIC_ASCII, availableGpus,
-		Arrays.asList(new Column().header("Index").headerAlign(HorizontalAlign.CENTER)
-			.dataAlign(HorizontalAlign.CENTER).with(gpu -> Integer.toString(availableGpus.indexOf(gpu))),
-			new Column().header("Vendor").headerAlign(HorizontalAlign.CENTER)
-				.dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().vendor()),
-			new Column().header("Name").headerAlign(HorizontalAlign.CENTER)
-				.dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().name()))));
 
-	try (Scanner scanner = new Scanner(System.in)) {
+	printAvailableGpus(availableGpus);
+
+	try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
 	    // prompt the user to enter N
 	    System.out.println("Please enter N.");
 	    System.out.print("> ");
-	    final int n = scanner.nextInt();
-	    
-	    // prompt the user to enter the index of the GPU that should perform the computation
+	    final int n = Integer.parseInt(reader.readLine().trim());
+
+	    // prompt the user to enter the index of the GPU that should perform the
+	    // computation
 	    System.out.println("Please enter the index of the GPU that should be used.");
 	    System.out.print("> ");
-	    final int gpuIndex = scanner.nextInt();
-	    if (gpuIndex < 0) {
-		System.err.println("invalid input: index must not be <0");
-		System.exit(0);
-	    } else if (gpuIndex >= availableGpus.size()) {
-		System.err.printf("invalid input: index %d is too large for only %d available GPUs\n", gpuIndex,
-			availableGpus.size());
-		System.exit(0);
-	    }
-	    
+	    final int gpuIndex = Integer.parseInt(reader.readLine().trim());
+
 	    // configure the GpuSolver
 	    gpuSolver.setN(n);
 	    gpuSolver.gpuSelection().choose(availableGpus.get(gpuIndex));
 
-	    // define callbacks for when the CpuSolver starts and finishes and when it makes progress
+	    // define callbacks for when the CpuSolver starts and finishes and when it makes
+	    // progress
 	    gpuSolver.onStart(() -> System.out.printf("Starting GpuSolver for N=%d.\n", n));
 	    gpuSolver.onUpdate((progress, solutions, duration) -> System.out.printf("\t%.2f%% [%d solutions, %.2fs]\n",
 		    progress * 100, solutions, duration / 1000f));
@@ -62,7 +53,22 @@ public class SingleGpuExample {
 
 	    // start the solver
 	    gpuSolver.start();
+
+	} catch (NumberFormatException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
+
+    private static void printAvailableGpus(List<Gpu> availableGpus) {
+	System.out.println(AsciiTable.getTable(AsciiTable.BASIC_ASCII, availableGpus,
+		Arrays.asList(new Column().header("Index").headerAlign(HorizontalAlign.CENTER)
+			.dataAlign(HorizontalAlign.CENTER).with(gpu -> Integer.toString(availableGpus.indexOf(gpu))),
+			new Column().header("Vendor").headerAlign(HorizontalAlign.CENTER)
+				.dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().vendor()),
+			new Column().header("Name").headerAlign(HorizontalAlign.CENTER)
+				.dataAlign(HorizontalAlign.CENTER).with(gpu -> gpu.getInfo().name()))));
     }
 
 }
