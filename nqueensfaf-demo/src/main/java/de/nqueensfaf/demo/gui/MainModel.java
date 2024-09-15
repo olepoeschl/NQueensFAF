d93b2188fb00062bdc677bf9fca9bffea1d9dbba
opoeschl@hs-mittweida.de <opoeschl@hs-mittweida.de>
2024-09-15 16:38:55 +0200

    Bugfix: set fileOpened to false on solver reset

--- nqueensfaf-demo/src/main/java/de/nqueensfaf/demo/gui/MainModel.java
+++ nqueensfaf-demo/src/main/java/de/nqueensfaf/demo/gui/MainModel.java
@@ -56,6 +56,10 @@ class MainModel {
 	    public void solverTerminated() {
 		fileOpened = false;
 	    }
+	    @Override
+	    public void solverReset() {
+		fileOpened = false;
+	    }
 	});
 	
 	addPropertyChangeListener("progress", e -> {
