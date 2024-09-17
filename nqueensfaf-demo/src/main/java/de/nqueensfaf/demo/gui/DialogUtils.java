d621acb86c8016012ab1034587c84c38fc5e8117
opoeschl@hs-mittweida.de <opoeschl@hs-mittweida.de>
2024-09-17 21:19:14 +0200

    Util method for showing loadng dialog

--- nqueensfaf-demo/src/main/java/de/nqueensfaf/demo/gui/DialogUtils.java
+++ nqueensfaf-demo/src/main/java/de/nqueensfaf/demo/gui/DialogUtils.java
@@ -1,30 +1,48 @@
 package de.nqueensfaf.demo.gui;
 
 import java.awt.EventQueue;
+import java.awt.GridBagLayout;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class DialogUtils {
+class DialogUtils {
     
     private static JFrame jframe;
     
-    public static void setJFrame(JFrame context) {
+    static void setJFrame(JFrame context) {
 	jframe = context;
     }
     
-    public static void error(String message, Runnable action) {
+    static void error(String message, Runnable action) {
 	JOptionPane.showMessageDialog(jframe, message, "Error", JOptionPane.ERROR_MESSAGE);
 	if(action != null)
 	    EventQueue.invokeLater(action);
     }
     
-    public static void error(String message) {
+    static void error(String message) {
 	error(message, null);
     }
     
-    public static void info(String message, String title) {
+    static void info(String message, String title) {
 	JOptionPane.showMessageDialog(jframe, message, title, JOptionPane.INFORMATION_MESSAGE);
     }
     
+    static JDialog loading(String title) {
+	String loadingText = "Please wait...";
+	var lbl = new JLabel(loadingText);
+	
+	var panel = new JPanel(new GridBagLayout());
+	panel.add(lbl, new QuickGBC(0, 0).anchor(QuickGBC.ANCHOR_CENTER).insets(5, 5, 5, 5));
+	
+	var dialog = new JDialog(jframe, title, true);
+	dialog.setContentPane(panel);
+	dialog.pack();
+	dialog.setVisible(true);
+	
+	return dialog;
+    }
 }
