401cc38e7f03f2da52ebaafd3fec026b3d9fe867
olepoeschl <ole.poeschl@gmail.com>
2024-11-13 14:16:48 +0100

    3.0.1
    also showing the version and version date in the gui now (about-tab)
    [trigger-nightly-build]

--- nqueensfaf-demo/src/main/java/de/nqueensfaf/demo/Main.java
+++ nqueensfaf-demo/src/main/java/de/nqueensfaf/demo/Main.java
@@ -9,8 +9,8 @@ import picocli.CommandLine;
 
 public class Main {
     
-    public static final String VERSION = "3.0.0-SNAPSHOT";
-    public static final String VERSION_DATE = "???";
+    public static final String VERSION = "3.0.1";
+    public static final String VERSION_DATE = "13.11.2024";
 
     public static void main(String[] args) {
 	if (args.length == 0) {
