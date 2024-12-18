88d5a75b5c37fe71651e6256be3b54d2c05a79c3
olepoeschl <ole.poeschl@gmail.com>
2024-12-18 12:14:59 +0100

    Update Demo version and version date
    [trigger-nightly-build]

--- nqueensfaf-demo/src/main/java/de/nqueensfaf/demo/Main.java
+++ nqueensfaf-demo/src/main/java/de/nqueensfaf/demo/Main.java
@@ -9,8 +9,8 @@ import picocli.CommandLine;
 
 public class Main {
     
-    public static final String VERSION = "3.0.1";
-    public static final String VERSION_DATE = "13.11.2024";
+    public static final String VERSION = "3.0.2";
+    public static final String VERSION_DATE = "18.12.2024";
 
     public static void main(String[] args) {
 	if (args.length == 0) {
